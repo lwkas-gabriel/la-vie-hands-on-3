d526b89f7257854fe4892d3da18fbaf9066040c8
Lucas Gabriel <lwkasgabriel@hotmail.com>
2022-05-22 16:04:08 -0300

    modelagem psicologo

---
+++ src/model/Psicologo.js
@@ -0,0 +1,30 @@
+const db = require("../database");
+const { DataTypes } = require("sequelize");
+
+const Psicologos = db.define("psicologos", {
+    id_psicologo:{
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    nome:{
+        type: DataTypes.STRING,
+    },
+    senha:{
+        type: DataTypes.STRING,
+        unique: true,
+    },
+    apresentacao:{
+        type: DataTypes.STRING,
+    },
+    createdAt:{
+        type: DataTypes.DATE,
+    },
+    updatedAt:{
+        type: DataTypes.DATE,
+    }
+},
+    {
+        tableName: "psicologos"
+    }
+);
