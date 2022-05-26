3977e654b7d8c0be429c0226b25994976bb1fa4c
Lucas Gabriel <lwkasgabriel@hotmail.com>
2022-05-26 11:33:14 -0300

    adicionando validação do put nas rotas

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,8 @@ const atendimentoController = require("../controllers/atendimentosController");
 const authController = require("../controllers/authController");
 const psicologoCreate = require("../validations/psicologos/create");
 const pacienteCreate = require("../validations/pacientes/create");
+const pacienteEdit = require("../validations/pacientes/edit");
+const psicologoEdit = require("../validations/psicologos/edit");
 const authLoginValidation = require("../validations/auth/login");
 const auth = require("../middlewares/auth");
 
@@ -19,14 +21,14 @@ routes.get("/pacientes", pacienteController.listarTodos);
 routes.get("/pacientes/:id_paciente", pacienteController.buscarPorId);
 routes.post("/pacientes", pacienteCreate, pacienteController.cadastrar);
 routes.delete("/pacientes/:id_paciente", pacienteController.deletar);
-routes.put("/pacientes/:id_paciente", pacienteController.atualizar);
+routes.put("/pacientes/:id_paciente", pacienteEdit ,pacienteController.atualizar);
 
 //psicologos
 routes.get("/psicologos", psicologoController.listarPsico);
 routes.get("/psicologos/:id_psicologo", psicologoController.listarId);
 routes.post("/psicologos", psicologoCreate,psicologoController.cadastrarPsicologo);
 routes.delete("/psicologos/:id_psicologo", psicologoController.deletarPsico);
-routes.put("/psicologos/:id_psicologo", psicologoController.atualizarPsico);
+routes.put("/psicologos/:id_psicologo", psicologoEdit, psicologoController.atualizarPsico);
 
 //atendimento
 routes.get("/atendimentos/:paciente_id", atendimentoController.listarId); //lista pelo id do paciente
